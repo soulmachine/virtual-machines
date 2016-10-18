925f7bfda2431db3d3e5988565381a4ed2564176
soulmachine <soulmachine@gmail.com>
2016-10-17 17:26:47 -0700

    Switched to OpenJDK

--- ubuntu/packer-templates/scripts/openjdk.sh
+++ ubuntu/packer-templates/scripts/openjdk.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "#################### Install OpenJDK 8 ####################"
-sudo apt -qy update -y
+sudo apt -qy update
 sudo apt -qy install openjdk-8-jdk
-sudo sh -c "echo 'export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /etc/profile"
-sudo sh -c "echo 'export CLASSPATH=\$JAVA_HOME/lib/*:\$JAVA_HOME/jre/lib/*' >> /etc/profile"
-sudo sh -c "echo 'export PATH=\$PATH:\$JAVA_HOME/bin' >> /etc/profile"
+echo -e "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64\nexport PATH=\$PATH:\$JAVA_HOME/bin\nexport CLASSPATH=\$JAVA_HOME/lib/*:\$JAVA_HOME/jre/lib/*\n" | sudo tee /etc/profile.d/openjdk.sh
