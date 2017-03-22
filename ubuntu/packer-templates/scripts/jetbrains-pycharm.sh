4feb6e7fa15b15ddbd7833e999a52846053968c9
soulmachine <soulmachine@gmail.com>
2017-03-22 01:55:10 -0700

    Upgraded PyCharm

--- ubuntu/packer-templates/scripts/jetbrains-pycharm.sh
+++ ubuntu/packer-templates/scripts/jetbrains-pycharm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "#################### Install JetBrains WebStorm ####################"
-pycharm_version="2016.3.2"
+pycharm_version="2016.3.3"
 wget https://download.jetbrains.com/python/pycharm-professional-${pycharm_version}.tar.gz -P ~/Downloads/
 sudo tar -zxf ~/Downloads/pycharm-professional-${pycharm_version}.tar.gz -C /opt
 rm ~/Downloads/*
 tmp_array=( /opt/pycharm-* )
 path=${tmp_array[0]}
-echo -e "[Desktop Entry]\nName=JetBrains PyCharm\nIcon=$path/bin/pycharm.png\nExec=\"$path/bin/pycharm.sh\" %f\nVersion=${pycharm_version}\nType=Application\nComment=Develop with pleasure\nCategories=Development;IDE;\nTerminal=false\nStartupWMClass=jetbrains-pycharm\nStartupNotify=true\n" | sudo tee  /usr/share/applications/jetbrains-pycharm.desktop
+echo -e "[Desktop Entry]\nVersion=${pycharm_version}\nType=Application\nName=PyCharm\nIcon=$path/bin/pycharm.png\nExec=\"$path/bin/pycharm.sh\" %f\nComment=The Drive to Develop\nCategories=Development;IDE;\nTerminal=false\nStartupWMClass=jetbrains-pycharm\n" | sudo tee  /usr/share/applications/jetbrains-pycharm.desktop
 sudo chmod u+x /usr/share/applications/jetbrains-pycharm.desktop
 
