8d3a4bf407c36c04157d643b2cf58bcdb55f5e97
soulmachine <soulmachine@gmail.com>
2022-08-29 03:01:09 +0000

    Fixed live share

--- lxd-images/scripts/vsls.sh
+++ lxd-images/scripts/vsls.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "#################### Install Linux prerequisites for vscode Live share ####################"
 
+# see https://github.com/MicrosoftDocs/live-share/issues/4646
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+sudo apt update
+sudo apt -qy install libssl1.1
+
 # see https://docs.microsoft.com/en-us/visualstudio/liveshare/reference/linux
 wget -O ~/vsls-reqs https://aka.ms/vsls-linux-prereq-script && chmod +x ~/vsls-reqs && ~/vsls-reqs && rm ~/vsls-reqs
 
-# see https://github.com/MicrosoftDocs/live-share/issues/4646
-echo "deb http://security.ubuntu.com/ubuntu impish-security main" | sudo tee /etc/apt/sources.list.d/impish-security.list
-sudo apt update
-sudo apt -qy install libssl1.1
