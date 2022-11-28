a4d14ae8a451c0b7356ad6cf9c3cf63b0847522c
soulmachine <soulmachine@gmail.com>
2022-11-28 01:55:36 +0000

    rust-analyzer-preview offically becomes rust-analyzer

--- lxd-images/scripts/rust.sh
+++ lxd-images/scripts/rust.sh
@@ -6,7 +6,7 @@ export CARGO_HOME=/opt/rust/cargo
 mkdir -p $RUSTUP_HOME
 mkdir -p $CARGO_HOME
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --default-toolchain nightly --component rust-src --component rust-analyzer-preview
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path --default-toolchain nightly --component rust-src --component rust-analyzer
 
 echo "export RUSTUP_HOME=$RUSTUP_HOME" >> /etc/profile
 echo "export PATH=\$PATH:$CARGO_HOME/bin" >> /etc/profile
